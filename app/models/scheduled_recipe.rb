61eef643271666bf9ec2ee427b9d6665c10149df
jeremy beaussart <jeremy.beaussart@gmail.com>
2025-09-07 09:37:54 +0200

    update anotate schema information for schedule recipe

--- app/models/scheduled_recipe.rb
+++ app/models/scheduled_recipe.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: scheduled_recipes
+#
+#  id          :bigint           not null, primary key
+#  day_of_week :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  recipe_id   :bigint           not null
+#  user_id     :bigint           not null
+#
+# Indexes
+#
+#  index_scheduled_recipes_on_recipe_id                (recipe_id)
+#  index_scheduled_recipes_on_user_id                  (user_id)
+#  index_scheduled_recipes_on_user_id_and_day_of_week  (user_id,day_of_week) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (recipe_id => recipes.id)
+#  fk_rails_...  (user_id => users.id)
+#
 class ScheduledRecipe < ApplicationRecord
   belongs_to :user
   belongs_to :recipe
