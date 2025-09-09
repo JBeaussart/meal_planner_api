f3f61c738c4a584496aabbd32843a02f60d66a24
jeremy beaussart <jeremy.beaussart@gmail.com>
2025-09-09 12:17:26 +0200

    fix authenticate_user

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -2,7 +2,7 @@
 module Api
   module V1
     class RecipesController < ApplicationController
-      before_action :authenticate_user!
+      before_action :authenticate_api_v1_user!
       before_action :set_recipe, only: %i[show update destroy]
 
       def index
